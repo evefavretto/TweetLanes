806b87b3656667880ca885948abc18cf6a758755
evefavretto <evefavretto@gmail.com>
2019-04-11 08:11:58 -0300

    Update ConsumerKeyConstants.java

--- android/app/src/main/java/com/tweetlanes/android/core/ConsumerKeyConstants.java
+++ android/app/src/main/java/com/tweetlanes/android/core/ConsumerKeyConstants.java
@@ -6,7 +6,7 @@ public class ConsumerKeyConstants {
 
     public static final String TWITTER_CONSUMER_KEY = "Put your key here";
     public static final String TWITTER_CONSUMER_SECRET = "Put your secret here";
-    public static final String APPDOTNET_CONSUMER_KEY = "Put your key here";
-    public static final String APPDOTNET_CONSUMER_SECRET = "Put your secret here";
+    public static final String APPDOTNET_CONSUMER_KEY = "R_C07mNlTEkVHGBxBu7Ky2-jXi1qcZzV";
+    public static final String APPDOTNET_CONSUMER_SECRET = "Zdbl0nN3buEwY73R-zRO3xeQENB9iVcT";
     public static final String CRITTERCISM_APP_ID = "4f6d53e0b093157b43000033";
 }
